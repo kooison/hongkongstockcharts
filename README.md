878ca5a0547c4ad5abd18974bba393648466f427
Wilson Wong <kooison@yahoo.com>
2017-05-05 18:31:53 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ If you want to monitor your portfolios in office but don't want to open aastocks
 Just download 3 files and open in local browser, that's it!
 
 # Screen shot 
-![Screen shot](https://github.com/kooison/hongkongstockcharts/blob/master/screenshot.png?raw=true "Screen shot")
+Night Mode Off
+![Night Mode Off](https://github.com/kooison/hongkongstockcharts/blob/master/day.png?raw=true "Night Mode Off")
+
+Night Mode On
+![Night Mode On](https://github.com/kooison/hongkongstockcharts/blob/master/night.png?raw=true "Night Mode On")
 
 # Sample
 [Link](https://kooison.github.io/hongkongstockcharts/stock.html)
--- stock.js
+++ stock.js
@@ -14,14 +14,14 @@ $.fn.fade = function () {
 	});
 };
 
-function createCharts(row, stockNum) {
+function createCharts(row, stockNum, scheme) {
 	var output = "<tr>";
 	var newUrl;
 	var id;
 	if (stockNum != undefined) {
 		$.each(CHARTTYPES, function (key, chart) {
 			id = row + "_" + key;
-			newUrl = chart.url.format(stockNum) + '&chartwidth=' + CONFIG.imgWidth + '&chartheight=' + CONFIG.imgHeight + '&ttt=' + new Date().getTime();
+			newUrl = chart.url.format(stockNum, scheme) + '&chartwidth=' + CONFIG.imgWidth + '&chartheight=' + CONFIG.imgHeight + '&ttt=' + new Date().getTime();
 			if (chart.active) {
 				output = output + TEMPLATES.img_holder.format(id, newUrl, id);
 			}
@@ -53,25 +53,44 @@ var CHARTTYPES = {
 	// charts are ordered
 	three_month: {
 		active: true,
-		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=T&lang=1&titlestyle=1&vol=1&scheme=1&com=100&stockid={0}.HK&period=7&type=1&logoStyle=1",
+		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=T&lang=1&titlestyle=1&vol=1&scheme={1}&com=100&stockid={0}.HK&period=7&type=1&logoStyle=1",
 	},
 	ten_day_per_one_hr: {
 		active: true,
-		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=T&lang=1&titlestyle=1&vol=1&scheme=1&com=100&stockid={0}.HK&period=4&type=1&logoStyle=1",
+		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=T&lang=1&titlestyle=1&vol=1&scheme={1}&com=100&stockid={0}.HK&period=4&type=1&logoStyle=1",
 	},
 	one_day_per_one_min: {
 		active: true,
-		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=F&lang=1&titlestyle=1&vol=1&scheme=1&com=100&stockid={0}.HK&period=5000&type=1&logoStyle=1",
+		url: "http://charts.aastocks.com/servlet/Charts?fontsize=11&15MinDelay=F&lang=1&titlestyle=1&vol=1&scheme={1}&com=100&stockid={0}.HK&period=5000&type=1&logoStyle=1",
 	}
 };
+
+var NIGHTMODE_SCHEME = {
+	ON: '3',
+	OFF: '1'
+}
 
 $(document).ready(function () {
 	if (!CONFIG.logEnable) {
 		console.log = function () {};
 	}
 
+	var scheme;
+	var container = $('.default');
+	console.log(container);
+	if (CONFIG.nightMode) {
+		scheme = NIGHTMODE_SCHEME.ON;
+		container.toggleClass('darkSwitch');
+		console.log('1 ' + CONFIG.nightMode);
+	} else {
+		scheme = NIGHTMODE_SCHEME.OFF;
+		container.toggleClass('lightSwitch');
+		console.log('2 ' + CONFIG.nightMode);
+	}
+	console.log(scheme);
+
 	$.each(CONFIG.stockList, function (row, stockNum) {
-		$('#stockList > tbody:last-child').append(createCharts(row, stockNum));
+		$('#stockList > tbody:last-child').append(createCharts(row, stockNum, scheme));
 	});
 
 	setInterval(function () {
